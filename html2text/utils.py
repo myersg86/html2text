47118787160e654130660b78e115af6dee1cc600
pavkazzz <pavel-mosein@yandex.ru>
2018-07-27 15:57:18 +0300

    remove escaping of dash

--- html2text/utils.py
+++ html2text/utils.py
@@ -243,7 +243,7 @@ def escape_md_section(text, snob=False, href=False):
 
     # text = config.RE_MD_DOT_MATCHER.sub(r"\1\\\2", text)
     # text = config.RE_MD_PLUS_MATCHER.sub(r"\1\\\2", text)
-    text = config.RE_MD_DASH_MATCHER.sub(r"\1\\\2", text)
+    # text = config.RE_MD_DASH_MATCHER.sub(r"\1\\\2", text)
 
     return text
 
